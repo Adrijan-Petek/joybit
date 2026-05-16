2c84c08a4ce16dba6b373bc7c2dfc6cf0c85bae6
Adrijan-Petek <adrijangithub@gmail.com>
2026-05-17 00:03:32 +0200

    chore: add talent verification meta tag

---
+++ app/head.tsx
@@ -0,0 +1,10 @@
+export default function Head() {
+  return (
+    <>
+      <meta
+        name="talentapp:project_verification"
+        content="8fd1a32bcb3eb97a26378ecc625b52332b7c8d598ae0a25620a80f206d91891d54bf5d27e3c237fe9eb7a98ca7c2209fc192976e8a45415374baa73eace62608"
+      />
+    </>
+  )
+}
--- app/layout.tsx
+++ app/layout.tsx
@@ -36,6 +36,7 @@ export const metadata: Metadata = {
     images: ['/branding/logo.png'],
   },
   other: {
+    'talentapp:project_verification': '8fd1a32bcb3eb97a26378ecc625b52332b7c8d598ae0a25620a80f206d91891d54bf5d27e3c237fe9eb7a98ca7c2209fc192976e8a45415374baa73eace62608',
     'fc:miniapp': JSON.stringify({
       version: "1",
       imageUrl: "https://joybit.vercel.app/branding/logo.png",
